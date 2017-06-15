dc44d871a258e9507770d5bf46de50055b0901e8
DawnShen <dawn414@126.com>
2017-06-15 17:12:14 +0800

    根据taxiid获取对应司机
    写在了trackjs和trackdao里面

--- server/dao/trackDao.js
+++ server/dao/trackDao.js
@@ -59,6 +59,29 @@ module.exports = {
             });
         });
     },
+
+    taxiIDtoDriver: function (req, res, next) {
+        pool.getConnection(function(err, connection) {
+            var param = req.query || req.params;
+            console.log('param is');
+            console.log(param);
+            connection.query('select * from driver where driverID in(select driverID from taxi where Taxiid='+param.id+')', function(err, result) {
+                if(err){
+                    console.log(err);
+                }
+                if(result) {
+                    console.log(result);
+                    console.log('query success');
+                }
+                jsonWrite(res, result);
+                connection.release();
+            });
+        });
+    },
+
+
+
+
     getpricebyorder: function (req, res, next) {
         pool.getConnection(function(err, connection) {
             var param = req.query || req.params;
