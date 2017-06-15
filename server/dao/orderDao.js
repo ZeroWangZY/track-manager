8cd61c88bc8a59f2d056f678ee70c8d171e0d1aa
DawnShen <dawn414@126.com>
2017-06-15 19:17:56 +0800

    orderRoute更新
    关于order界面Route的更新

---
+++ server/dao/orderDao.js
@@ -0,0 +1,96 @@
+
+var mysql = require('mysql');
+var $conf = require('../conf/db');
+var pool  = mysql.createPool($conf.mysql);
+
+var jsonWrite = function (res, ret) {
+    if(typeof ret === 'undefined') {
+        res.json({
+            code:'1',
+            msg: '操作失败'
+        });
+    } else if(ret.length == 0) {
+        res.json({
+            code:'1',
+            msg:'not found'
+        });
+    } else {
+            res.json(ret)
+    }
+};
+
+
+module.exports = {
+    getOrderCount: function (req, res, next) {
+        pool.getConnection(function(err, connection) {
+            var param = req.query || req.params;
+            connection.query('SELECT count(*) as count from order', function(err, result) {
+                if(err){
+                    console.log(err);
+                }
+                if(result) {
+                    console.log(result);
+                    console.log('query success');
+                }
+                jsonWrite(res, result);
+                connection.release();
+            });
+        });
+    },
+
+    getAllOrder: function (req ,res, next) {
+      pool.getConnection(function(err, connection) {
+          connection.query('SELECT * from order', function(err, result) {
+              if(err){
+                  console.log(err);
+              }
+              if(result) {
+                  console.log('query success');
+              }
+              jsonWrite(res, result);
+              connection.release();
+          });
+      });
+    },
+
+
+    getOrderByID: function (req, res, next) {
+        pool.getConnection(function (err, connection) {
+            var param = req.query || req.params;
+            console.log('param is');
+            console.log(param);
+            connection.query('SELECT * from order where Orderid='+ param.id , function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                if (result) {
+                    console.log(result);
+                    console.log('query success');
+                }
+                jsonWrite(res, result);
+                connection.release();
+            });
+        });
+    },
+
+    getOrdersByTaxiID: function (req, res, next) {
+        pool.getConnection(function (err, connection) {
+            var param = req.query || req.params;
+            console.log('param is');
+            console.log(param);
+            connection.query('SELECT * from order where Taxiid like \'%'+ param.id+'%\'' , function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                if (result) {
+                    console.log(result);
+                    console.log('query success');
+                }
+                jsonWrite(res, result);
+                connection.release();
+            });
+        });
+    }
+
+
+};
---
+++ server/routes/order.js
@@ -0,0 +1,25 @@
+
+var express = require('express');
+var router = express.Router();
+var orderDao = require('../dao/orderDao');
+var path = require('path');
+
+router.get('/getordercount', function(req, res, next) {
+    orderDao.getOrderCount(req,res,next);
+});
+
+
+router.get('/getallorder', function(req, res, next) {
+    orderDao.getAllOrder(req,res,next);
+});
+
+router.get("getorderbyid", function(req, res, next) {
+    orderDao.getOrderByID(req,res,next);
+});
+
+router.get('/getorderbytaxiid', function(req, res, next) {
+    orderDao.getOrdersByTaxiID(req,res,next);
+});
+
+
+module.exports = router;
